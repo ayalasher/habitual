85ab461e320c6af1fc4e81a9cfa50469f533a1be
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-06-25 13:26:49 +0300

    Moved API keys to the ENV file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
-import ReactLoading from "react-loading";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
@@ -16,21 +15,22 @@ export default function HomeScreen() {
   const auth = getAuth();
   const navigate = useNavigate();
   const currentUser = auth.currentUser;
+  const url = import.meta.env.VITE_URL;
+  const x_rapidapi_key = import.meta.env.VITE_X_RAPID_API_KEY;
+  const x_rapidapi_host = import.meta.env.VITE_X_RAPID_API_HOST;
 
   useEffect(() => {
     if (currentUser) {
       async function getapi() {
         const options = {
           method: "GET",
-          url: "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote",
+          url: `${url}`,
           params: {
             token: "ipworld.info",
           },
           headers: {
-            "x-rapidapi-key":
-              "4f2abe3dc3msh815fb3afb4ca166p167c42jsna2f5bc555856",
-            "x-rapidapi-host":
-              "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com",
+            "x-rapidapi-key": `${x_rapidapi_key}`,
+            "x-rapidapi-host": `${x_rapidapi_host}`,
           },
         };
         try {
@@ -46,14 +46,13 @@ export default function HomeScreen() {
 
       // const api_url = "https://zenquotes.io/api/today/";
       getapi();
-      toast.info(`UUID${currentUser.uid}`);
     } else {
       toast.error("You must be logged in to view the homepage");
       navigate("/");
     }
   }, []);
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-12/12">
       <div>
         <p className="text-center font-bold text-3xl font-mono my-3 px-2 py-2 ">
           Today's Inspiration
