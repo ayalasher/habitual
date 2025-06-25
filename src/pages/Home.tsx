5cff46c4932e32a040b7c09a7a42a3766078a6b5
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-06-25 11:46:25 +0300

    Added the 404 error page and ensured Home page in accessed by authenticated users only

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
 import ReactLoading from "react-loading";
+import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 interface Quote {
   author: string;
@@ -11,35 +13,44 @@ interface Quote {
 export default function HomeScreen() {
   const [todaysQuote, setTodaysquote] = useState<Quote | null>(null);
   const [loading, setLoading] = useState(false);
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const currentUser = auth.currentUser;
 
   useEffect(() => {
-    async function getapi() {
-      const options = {
-        method: "GET",
-        url: "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote",
-        params: {
-          token: "ipworld.info",
-        },
-        headers: {
-          "x-rapidapi-key":
-            "4f2abe3dc3msh815fb3afb4ca166p167c42jsna2f5bc555856",
-          "x-rapidapi-host":
-            "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com",
-        },
-      };
-      try {
-        setLoading(true);
-        const response = await axios.request(options);
-        setTodaysquote(response.data);
-        setLoading(false);
-      } catch (error: any) {
-        setLoading(false);
-        toast.error(`An error occured getting today's inspiration.`);
+    if (currentUser) {
+      async function getapi() {
+        const options = {
+          method: "GET",
+          url: "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote",
+          params: {
+            token: "ipworld.info",
+          },
+          headers: {
+            "x-rapidapi-key":
+              "4f2abe3dc3msh815fb3afb4ca166p167c42jsna2f5bc555856",
+            "x-rapidapi-host":
+              "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com",
+          },
+        };
+        try {
+          setLoading(true);
+          const response = await axios.request(options);
+          setTodaysquote(response.data);
+          setLoading(false);
+        } catch (error: any) {
+          setLoading(false);
+          toast.error(`An error occured getting today's inspiration.`);
+        }
       }
-    }
 
-    // const api_url = "https://zenquotes.io/api/today/";
-    getapi();
+      // const api_url = "https://zenquotes.io/api/today/";
+      getapi();
+      toast.info(`UUID${currentUser.uid}`);
+    } else {
+      toast.error("You must be logged in to view the homepage");
+      navigate("/");
+    }
   }, []);
   return (
     <div className="flex flex-col">
